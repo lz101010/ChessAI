363dc884e15b3cb88f14f5ca325c5bb57fefa912
Lukas Zeller <5059085+lz101010@users.noreply.github.com>
2022-02-27 08:06:54 +0100

    :sparkles: add basic data types
    * :sparkles: add basic data types
    * :white_check_mark: increase test coverage

---
+++ src/main/kotlin/com/lz101010/chess/data/Board.kt
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 Lukas Zeller
+
+package com.lz101010.chess.data
+
+import com.lz101010.chess.data.PieceType.R
+import com.lz101010.chess.data.PieceType.N
+import com.lz101010.chess.data.PieceType.B
+import com.lz101010.chess.data.PieceType.P
+import com.lz101010.chess.data.PieceType.Q
+import com.lz101010.chess.data.PieceType.K
+import java.util.*
+
+private val INITIAL_BOARD_LAYOUT = arrayOf(
+    arrayOf<Piece?>(R.asBlack, N.asBlack, B.asBlack, Q.asBlack, K.asBlack, B.asBlack, N.asBlack, R.asBlack),
+    arrayOf<Piece?>(P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack),
+    arrayOf<Piece?>(null, null, null, null, null, null, null, null),
+    arrayOf<Piece?>(null, null, null, null, null, null, null, null),
+    arrayOf<Piece?>(null, null, null, null, null, null, null, null),
+    arrayOf<Piece?>(null, null, null, null, null, null, null, null),
+    arrayOf<Piece?>(P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite),
+    arrayOf<Piece?>(R.asWhite, N.asWhite, B.asWhite, Q.asWhite, K.asWhite, B.asWhite, N.asWhite, R.asWhite)
+)
+
+private val EMPTY_BOARD_LAYOUT = arrayOf(
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8)
+)
+
+data class Board(
+    val pieces: Array<Array<Piece?>> = INITIAL_BOARD_LAYOUT,
+    val whiteToMove: Boolean = true
+) {
+    companion object {
+        var empty = Board(pieces = EMPTY_BOARD_LAYOUT)
+    }
+    fun evalScore(): Int {
+        return pieces.flatten()
+            .filterNotNull()
+            .filter { whiteToMove == it.white }
+            .sumOf { it.value }
+    }
+
+    operator fun get(field: Field): Piece? {
+        val row = field.ordinal / 8
+        val col = field.ordinal % 8
+        return pieces[row][col]
+    }
+
+    override fun toString(): String {
+        return pieces.joinToString("\n") { row -> row.joinToString(" ") { it?.basic ?: "-" } }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        return pieces.contentDeepEquals((other as Board).pieces) && whiteToMove == other.whiteToMove
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(pieces.contentDeepHashCode(), whiteToMove)
+    }
+}
---
+++ src/test/kotlin/com/lz101010/chess/data/BoardTest.kt
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2022 Lukas Zeller
+
+package com.lz101010.chess.data
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BoardTest {
+    @Test
+    fun initialBoardPrints_passes() {
+        assertThat(Board().toString()).isEqualTo("""
+            r n b q k b n r
+            p p p p p p p p
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            P P P P P P P P
+            R N B Q K B N R
+        """.trimIndent())
+    }
+
+    @Test
+    fun equality_passes() {
+        val board1 = Board()
+        val board2 = Board()
+        assertThat(board1).isEqualTo(board1)
+        assertThat(board1).isEqualTo(board2)
+        assertThat(board1.hashCode()).isEqualTo(board2.hashCode())
+
+        assertThat(board1).isNotEqualTo(PieceType.B)
+        assertThat(board1).isNotEqualTo(Board.empty)
+        assertThat(board1).isNotEqualTo(null)
+        assertThat(Board(whiteToMove = true)).isNotEqualTo(Board(whiteToMove = false))
+        assertThat(Board(whiteToMove = true).hashCode()).isNotEqualTo(Board(whiteToMove = false).hashCode())
+    }
+
+    @Test
+    fun initialEvalScore_passes() {
+        val initialScore = 8 + 9 + 6 + 6 + 10
+        assertThat(Board(whiteToMove = true).evalScore()).isEqualTo(initialScore)
+        assertThat(Board(whiteToMove = false).evalScore()).isEqualTo(initialScore)
+    }
+
+    @Test
+    fun initialBoardColors_passes() {
+        for (index in 0..7) {
+            assertThat(Board().pieces[0][0]!!.black).isTrue
+            assertThat(Board().pieces[1][0]!!.black).isTrue
+            assertThat(Board().pieces[2][0]).isNull()
+            assertThat(Board().pieces[3][0]).isNull()
+            assertThat(Board().pieces[4][0]).isNull()
+            assertThat(Board().pieces[5][0]).isNull()
+            assertThat(Board().pieces[6][0]!!.white).isTrue
+            assertThat(Board().pieces[7][0]!!.white).isTrue
+        }
+    }
+}
