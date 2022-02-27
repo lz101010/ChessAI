600fe8f26a81f67118909d21ad6607406bdf7c26
Lukas Zeller <l.zeller92@gmail.com>
2022-02-27 06:30:54 +0100

    :white_check_mark: increase test coverage

--- src/main/kotlin/com/lz101010/chess/data/Board.kt
+++ src/main/kotlin/com/lz101010/chess/data/Board.kt
@@ -12,20 +12,34 @@ import com.lz101010.chess.data.PieceType.K
 import java.util.*
 
 private val INITIAL_BOARD_LAYOUT = arrayOf(
-    arrayOf(R.asBlack, N.asBlack, B.asBlack, Q.asBlack, K.asBlack, B.asBlack, N.asBlack, R.asBlack),
-    arrayOf(P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack),
+    arrayOf<Piece?>(R.asBlack, N.asBlack, B.asBlack, Q.asBlack, K.asBlack, B.asBlack, N.asBlack, R.asBlack),
+    arrayOf<Piece?>(P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack, P.asBlack),
     arrayOf<Piece?>(null, null, null, null, null, null, null, null),
     arrayOf<Piece?>(null, null, null, null, null, null, null, null),
     arrayOf<Piece?>(null, null, null, null, null, null, null, null),
     arrayOf<Piece?>(null, null, null, null, null, null, null, null),
-    arrayOf(P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite),
-    arrayOf(R.asWhite, N.asWhite, B.asWhite, Q.asWhite, K.asWhite, B.asWhite, N.asWhite, R.asWhite)
+    arrayOf<Piece?>(P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite, P.asWhite),
+    arrayOf<Piece?>(R.asWhite, N.asWhite, B.asWhite, Q.asWhite, K.asWhite, B.asWhite, N.asWhite, R.asWhite)
+)
+
+private val EMPTY_BOARD_LAYOUT = arrayOf(
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8),
+    arrayOfNulls<Piece?>(8)
 )
 
 data class Board(
-    val pieces: Array<Array<out Piece?>> = INITIAL_BOARD_LAYOUT,
+    val pieces: Array<Array<Piece?>> = INITIAL_BOARD_LAYOUT,
     val whiteToMove: Boolean = true
 ) {
+    companion object {
+        var empty = Board(pieces = EMPTY_BOARD_LAYOUT)
+    }
     fun evalScore(): Int {
         return pieces.flatten()
             .filterNotNull()
